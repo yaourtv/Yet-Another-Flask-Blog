16534835af21742919803c5818f1ab67c58037bd
Yaourt Ongit <yaourt.ongit@gmail.com>
2019-08-19 14:50:09 +0300

    Add custom error pages.

--- flaskproj/__init__.py
+++ flaskproj/__init__.py
@@ -24,9 +24,11 @@ def create_app(config_class=Config):
 	from flaskproj.main.routes import main
 	from flaskproj.users.routes import users
 	from flaskproj.posts.routes import posts
+	from flaskproj.errors.handlers import errors
 
 	app.register_blueprint(main)
 	app.register_blueprint(posts)
 	app.register_blueprint(users)
+	app.register_blueprint(errors)
 
 	return app
