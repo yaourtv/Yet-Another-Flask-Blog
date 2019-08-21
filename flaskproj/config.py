94ee09958903e0104f0dc53489072a82ad093ce0
Yaourt Ongit <yaourt.ongit@gmail.com>
2019-08-21 16:38:57 +0300

    Generate SQLAlchemy to PostgreSQL migration
    Since Heroku requires a lot of changes to a project I decided to add a specific branch. Probably would do detailed guide "How to...", because it's not so clear as you think.

--- flaskproj/config.py
+++ flaskproj/config.py
@@ -3,10 +3,9 @@ import os
 class Config:
 
 	SECRET_KEY = 'f5636f0074bfa87953403c43b8c17701'
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///project.db'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 	MAIL_SERVER = 'smtp.googlemail.com'
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
 	MAIL_USERNAME = os.environ.get('EMAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-		
